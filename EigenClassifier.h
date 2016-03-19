de5558cd5e96008d132e6b6caabb0884d0d279c2
Prakash <prakraja@umail.iu.edu>
2016-03-19 14:20:16 -0400

    The classify method in EigenClassifier

--- EigenClassifier.h
+++ EigenClassifier.h
@@ -11,7 +11,7 @@
 
 using namespace cimg_library;
 using namespace std;
-
+// Instructions from http://blog.manfredas.com/eigenfaces-tutorial
 class EigenClassifier : public Classifier {
 public:
   EigenClassifier(const vector<string> &_class_list) : Classifier(_class_list) {}
@@ -25,17 +25,8 @@ public:
       // THIS CREATES THE BIG MATRIX
       cout << " Total Number of training images are " << c_iter->second.size() << "\n";
       for(int i=0; i<c_iter->second.size(); i++) {
-	CImg<double> e = extract_features(c_iter->second[i].c_str());
-	cout<< "EImage w:h of is " << e._width  << " : " << e._height << "\n";
-	double tot=0,avg = 0;
-	cimg_forY(e,y) {
-	  tot += e(0,y,0);
-	}
-	avg = tot/e._height;
-	cimg_forY(e,y) {
-	  e(0,y,0) -= avg;
-	}
-        A.draw_image(i, 0, 0, 0, e);
+	CImg<double> e = get_mean_adjusted_image(extract_features(c_iter->second[i].c_str()));
+	A.draw_image(i, 0, 0, 0, e);
       }
       CImg<double> At = A.get_transpose();
       CImg<double> L = At * A;
@@ -64,17 +55,54 @@ public:
       U.save_png(("nn_model." + c_iter->first + ".png").c_str());
     }
   }
+  virtual CImg<double> get_mean_adjusted_image(CImg<double> img) {
+    CImg<double> e(img);
+    cout<< "Get_mean_adjusted_mage w:h of is " << e._width  << " : " << e._height << "\n";
+    double tot=0,avg = 0;
+    cimg_forC(e,c) {
+      tot += c;
+    }
+    avg = tot/e._height;
+    cimg_forXY(e,x,y) {
+      e(x,y) -= avg;
+    }
+    return e;
+  }
 
-  virtual string classify(const string &filename)
-  {
+  virtual string classify(const string &filename) {
     CImg<double> test_image = extract_features(filename);
-    // figure nearest neighbor
-    pair<string, double> best("", 10e100);
-    double this_cost;
-    return best.first;
+    // Now let get Ut * (Meaned out test_image)
+    // phi is meaned out test_image
+    CImg<double> phi = get_mean_adjusted_image(test_image);
+    double threshold = 0.2;
+    string ret = "";
+    double min_minval = 100;
+    for(int c=0; c < class_list.size(); c++) {
+      CImg<double> omega = models[class_list[c]].get_transpose() * phi;
+      // Omega should be a huge vector with weights for each image
+      // If euclidean weight for any weight is lower than threshold then things work
+      double minval = 100,val;
+      cimg_forC(omega,C) {
+	val = sqrt((C - threshold) * (C - threshold));
+	if (val < minval) {
+	  minval = val;
+	}
+      }
+      if (minval < threshold) {
+	return class_list[c];
+      }
+      if (minval <= min_minval) {
+	ret = class_list[c];
+	min_minval = minval;
+      }
+    }
+    return ret;
   }
 
   virtual void load_model() {
+    // Get all the images from U
+    for(int c=0; c < class_list.size(); c++)
+      models[class_list[c] ] = (CImg<double>(("nn_model." + class_list[c] + ".png").c_str()));
   }
 protected:
   // extract features from an image, which in this case just involves resampling and
@@ -96,6 +124,6 @@ protected:
     return k;
   }
 
-  static const int size=10;  // subsampled image resolution
+  static const int size=40;  // subsampled image resolution
   map<string, CImg<double> > models; // trained models
 };
