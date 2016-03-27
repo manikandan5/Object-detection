0544a62a556b4f11e8c24d4ec76b8a928ad934f1
murugesm <murugesm@indiana.edu>
2016-03-27 18:24:24 -0400

    Update a3.cpp

--- a3.cpp
+++ a3.cpp
@@ -52,70 +52,111 @@ typedef map<string, vector<string> > Dataset;
 #include <SVM.h>
 #include <neural.h>
 #include <svm2.h>
+#include <BOW.h>
 
 // Figure out a list of files in a given directory.
 //
 vector<string> files_in_directory(const string &directory, bool prepend_directory = false)
 {
-  vector<string> file_list;
-  DIR *dir = opendir(directory.c_str());
-  if(!dir)
-    throw std::string("Can't find directory " + directory);
-  
-  struct dirent *dirent;
-  while ((dirent = readdir(dir))) 
-    if(dirent->d_name[0] != '.')
-      file_list.push_back((prepend_directory?(directory+"/"):"")+dirent->d_name);
-
-  closedir(dir);
-  return file_list;
+    vector<string> file_list;
+    DIR *dir = opendir(directory.c_str());
+    if(!dir)
+        throw std::string("Can't find directory " + directory);
+    
+    struct dirent *dirent;
+    while ((dirent = readdir(dir)))
+        if(dirent->d_name[0] != '.')
+            file_list.push_back((prepend_directory?(directory+"/"):"")+dirent->d_name);
+    
+    closedir(dir);
+    return file_list;
 }
 
 int main(int argc, char **argv)
 {
-  try {
-    if(argc < 3)
-      throw string("Insufficent number of arguments");
-
-    string mode = argv[1];
-    string algo = argv[2];
-
-    // Scan through the "train" or "test" directory (depending on the
-    //  mode) and builds a data structure of the image filenames for each class.
-    Dataset filenames; 
-    vector<string> class_list = files_in_directory(mode);
-    for(vector<string>::const_iterator c = class_list.begin(); c != class_list.end(); ++c)
-      filenames[*c] = files_in_directory(mode + "/" + *c, true);
-
-    // set up the classifier based on the requested algo
-    Classifier *classifier=0;
-    if(algo == "nn")
-      classifier = new NearestNeighbor(class_list);
-    else if(algo == "haar")
-      classifier = new Haar(class_list);
-    else if(algo == "svm")
-      classifier = new SVM(class_list);
-    else if(algo == "svm2")
-      classifier = new SVM2(class_list);
-    else if(algo == "ei")
-      classifier = new EigenClassifier(class_list);
-    else if(algo == "nu")
-      classifier = new Neural(class_list);
-    else
-      throw std::string("unknown classifier " + algo);
-
-    // now train or test!
-    if(mode == "train")
-      classifier->train(filenames);
-    else if(mode == "test")
-      classifier->test(filenames);
-    else
-      throw std::string("unknown mode!");
-  }
-  catch(const string &err) {
-    cerr << "Error: " << err << endl;
-  }
-
+    try {
+        bool buildData = true;
+        
+        if(argc < 3)
+            throw string("Insufficent number of arguments");
+        else if(argc>3)
+            buildData = argv[3];
+        
+        string mode = argv[1];
+        string algo = argv[2];
+        
+        // Scan through the "train" or "test" directory (depending on the
+        //  mode) and builds a data structure of the image filenames for each class.
+        Dataset filenames;
+        vector<string> class_list = files_in_directory(mode);
+        for(vector<string>::const_iterator c = class_list.begin(); c != class_list.end(); ++c)
+            filenames[*c] = files_in_directory(mode + "/" + *c, true);
+        
+        // set up the classifier based on the requested algo
+        Classifier *classifier=0;
+        if(algo == "nn")
+            classifier = new NearestNeighbor(class_list);
+        else if(algo == "haar")
+            classifier = new Haar(class_list);
+        else if(algo == "svm")
+            classifier = new SVM(class_list);
+        else if(algo == "svm2")
+            classifier = new SVM2(class_list);
+        else if(algo == "ei")
+            classifier = new EigenClassifier(class_list);
+        else if(algo == "nu")
+            classifier = new Neural(class_list);
+        else if(algo == "bow")
+            classifier = new BOW(class_list);
+        else
+            throw std::string("unknown classifier " + algo);
+        
+        // now train or test!
+        if(mode == "train")
+        {
+            if(algo!="bow")
+            {
+                classifier->train(filenames);
+            }
+            else
+            {
+                if(buildData)
+                {
+                    classifier->train(filenames);
+                    system("./svm_multiclass_learn -c 0.1 bow-train-features bow-model ");
+                }
+                else
+                {
+                    system("./svm_multiclass_learn -c 0.1 bow-train-features bow-model ");
+                }
+            }
+        }
+        else if(mode == "test")
+        {
+            if(algo!="bow")
+            {
+                classifier->test(filenames);
+            }
+            else
+            {
+                if(buildData)
+                {
+                    classifier->test(filenames);
+                    system("./svm_multiclass_classify bow-test-features bow-model bow-predictions");
+                }
+                else
+                {
+                    system("./svm_multiclass_classify bow-test-features bow-model bow-predictions");
+                }
+                
+            }
+        }
+        else
+            throw std::string("unknown mode!");
+    }
+    catch(const string &err) {
+        cerr << "Error: " << err << endl;
+    }
 }
 
 
